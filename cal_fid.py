4f25e9cce83407d792ee04c1fdbb5f75df2234d7
Aaron Xu <ylxu@mit.edu>
2022-12-25 23:32:58 -0500

    update generator

--- cal_fid.py
+++ cal_fid.py
@@ -9,8 +9,11 @@ def calculate_fid_from_inception_stats():
 
     mu = gen['mu']
     sigma = gen['sigma']
+    print(mu.shape, sigma.shape, mu_ref.shape, sigma_ref.shape)
     m = np.square(mu - mu_ref).sum()
+    print("h")
     s, _ = linalg.sqrtm(np.dot(sigma, sigma_ref), disp=False)
+    print('good')
     fid = m + np.trace(sigma + sigma_ref - s * 2)
     print("FID:", float(np.real(fid)))
     return float(np.real(fid))
