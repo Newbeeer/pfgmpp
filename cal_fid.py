c28eae648120743981415682ee0be27613097f47
Aaron Xu <ylxu@mit.edu>
2022-12-25 23:22:53 -0500

    update generator

---
+++ cal_fid.py
@@ -0,0 +1,6 @@
+def calculate_fid_from_inception_stats():
+    ref = dict(np.load(f))
+    m = np.square(mu - mu_ref).sum()
+    s, _ = scipy.linalg.sqrtm(np.dot(sigma, sigma_ref), disp=False)
+    fid = m + np.trace(sigma + sigma_ref - s * 2)
+    return float(np.real(fid))
--- fid.py
+++ fid.py
@@ -12,7 +12,7 @@ import click
 import tqdm
 import pickle
 import numpy as np
-import scipy.linalg
+from scipy import linalg
 import torch
 import dnnlib
 from torch_utils import distributed as dist
@@ -84,7 +84,7 @@ def calculate_inception_stats(
 def calculate_fid_from_inception_stats(mu, sigma, mu_ref, sigma_ref):
     print(mu.shape, sigma.shape, mu_ref.shape, sigma_ref.shape)
     m = np.square(mu - mu_ref).sum()
-    s, _ = scipy.linalg.sqrtm(np.dot(sigma, sigma_ref), disp=False)
+    s, _ = linalg.sqrtm(np.dot(sigma, sigma_ref), disp=False)
     fid = m + np.trace(sigma + sigma_ref - s * 2)
     return float(np.real(fid))
 
