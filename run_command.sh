60adf4ddbc6e8072ec1ef31747fc14eb657e5f18
Aaron Xu <ylxu@mit.edu>
2023-01-06 21:13:45 -0500

    update

--- run_command.sh
+++ run_command.sh
@@ -15,7 +15,10 @@ torchrun --standalone --nproc_per_node=8 train.py \
   --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
   --pfgmv2=1 --batch 512 --align=0 --aug_dim 3072000
 # edm
-
+torchrun --standalone --nproc_per_node=8 train.py \
+  --outdir=training-runs --name edm_ncsnpp \
+  --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
+  --batch 512
 
 # ===== generate =======
 # 512
@@ -31,4 +34,6 @@ torchrun --standalone --nproc_per_node=8 generate.py \
   --seeds=0-49999 --outdir=./training-runs/3072000_stf_0_align_0 \
   --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=3072000
 # edm
-
+torchrun --standalone --nproc_per_node=8 generate.py \
+  --seeds=0-49999 --outdir=./training-runs/edm_ncsnpp \
+  --ckpt 100000
