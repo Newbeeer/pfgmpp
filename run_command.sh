81dfe6af674a8307d0600073f2d7d80e945bac34
HobbitLong <yonglong@mit.edu>
2023-01-06 21:01:45 -0500

    add command

---
+++ run_command.sh
@@ -0,0 +1,33 @@
+# ===== training =======
+# 512
+torchrun --standalone --nproc_per_node=8 train.py \
+  --outdir=training-runs --name 512_stf_0_align_0 \
+  --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
+  --pfgmv2=1 --batch 512 --align=0 --aug_dim 512
+# 8192
+torchrun --standalone --nproc_per_node=8 train.py \
+  --outdir=training-runs --name 8192_stf_0_align_0 \
+  --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
+  --pfgmv2=1 --batch 512 --align=0 --aug_dim 8192
+# 3072000
+torchrun --standalone --nproc_per_node=8 train.py \
+  --outdir=training-runs --name 3072000_stf_0_align_0 \
+  --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
+  --pfgmv2=1 --batch 512 --align=0 --aug_dim 3072000
+# edm
+
+
+# ===== generate =======
+# 512
+torchrun --standalone --nproc_per_node=8 generate.py \
+  --seeds=0-49999 --outdir=./training-runs/512_stf_0_align_0 \
+  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=512
+# 8192
+torchrun --standalone --nproc_per_node=8 generate.py \
+  --seeds=0-49999 --outdir=./training-runs/8192_stf_0_align_0 \
+  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=8192
+# 3072000
+torchrun --standalone --nproc_per_node=8 generate.py \
+  --seeds=0-49999 --outdir=./training-runs/3072000_stf_0_align_0 \
+  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=3072000
+# edm
