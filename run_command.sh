fc2a2385563e5936478effdc05d5be965aebad38
Aaron Xu <ylxu@mit.edu>
2023-02-07 20:00:46 -0500

    update

--- run_command.sh
+++ run_command.sh
@@ -1,42 +1,19 @@
-# ===== stf training ====== #
-
-# 2048 stf
-torchrun --standalone --nproc_per_node=8 train.py \
-  --outdir=training-runs --name 2048_stf_1_align_0 \
-  --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
-  --pfgmv2=1 --batch 512 --align=0 --aug_dim 2048 --stf=1 --rbatch 1024
-  
-torchrun --standalone --nproc_per_node=8 generate.py \
-  --seeds=0-49999 --outdir=./training-runs/2048_stf_1_align_0 \
-  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=2048 --use_pickle=1
-
-# 2048 condition
-torchrun --standalone --nproc_per_node=8 train.py \
-  --outdir=training-runs --name 2048_cond_1 \
-  --data=datasets/cifar10-32x32.zip --cond=1 --arch=ncsnpp \
-  --pfgmv2=1 --batch 512 --align=0 --aug_dim 2048
-  
-torchrun --standalone --nproc_per_node=8 generate.py \
-  --seeds=0-49999 --outdir=./training-runs/2048_cond_1 \
-  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=2048 --use_pickle=1
-
-
-# ===== training =======
+# ===== training CIFAR-10 =======
 # 512
 torchrun --standalone --nproc_per_node=8 train.py \
-  --outdir=training-runs --name 512_stf_0_align_0 \
+  --outdir=training-runs --name 512 \
   --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
-  --pfgmv2=1 --batch 512 --align=0 --aug_dim 512
+  --pfgmpp=1 --batch 512 --aug_dim 512
 # 8192
 torchrun --standalone --nproc_per_node=8 train.py \
-  --outdir=training-runs --name 8192_stf_0_align_0 \
+  --outdir=training-runs --name 8192 \
   --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
-  --pfgmv2=1 --batch 512 --align=0 --aug_dim 8192
+  --pfgmpp=1 --batch 512 --aug_dim 8192
 # 3072000
 torchrun --standalone --nproc_per_node=8 train.py \
-  --outdir=training-runs --name 3072000_stf_0_align_0 \
+  --outdir=training-runs --name 3072000 \
   --data=datasets/cifar10-32x32.zip --cond=0 --arch=ncsnpp \
-  --pfgmv2=1 --batch 512 --align=0 --aug_dim 3072000
+  --pfgmpp=1 --batch 512 --aug_dim 3072000
 # edm
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 train.py \
   --outdir=training-runs --name edm_ncsnpp \
@@ -46,16 +23,16 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 tr
 # ===== generate =======
 # 512
 torchrun --standalone --nproc_per_node=8 generate.py \
-  --seeds=0-49999 --outdir=./training-runs/512_stf_0_align_0 \
-  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=512
+  --seeds=0-49999 --outdir=./training-runs/512 \
+  --ckpt 100000 --pfgmpp=1 --aug_dim=512
 # 8192
 torchrun --standalone --nproc_per_node=8 generate.py \
-  --seeds=0-49999 --outdir=./training-runs/8192_stf_0_align_0 \
-  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=8192
+  --seeds=0-49999 --outdir=./training-runs/8192 \
+  --ckpt 100000 --pfgmpp=1 --aug_dim=8192
 # 3072000
 torchrun --standalone --nproc_per_node=8 generate.py \
-  --seeds=0-49999 --outdir=./training-runs/3072000_stf_0_align_0 \
-  --ckpt 100000 --pfgmv2=1 --align=0 --aug_dim=3072000
+  --seeds=0-49999 --outdir=./training-runs/3072000 \
+  --ckpt 100000 --pfgmpp=1 --aug_dim=3072000
 # edm
 torchrun --standalone --nproc_per_node=8 generate.py \
   --seeds=0-49999 --outdir=./training-runs/edm_ncsnpp \
@@ -64,11 +41,11 @@ torchrun --standalone --nproc_per_node=8 generate.py \
 
 # ===== fid evaluation =====
 # 512
-torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/512_stf_0_align_0 --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_512_8GPU.txt
+torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/512 --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_512_8GPU.txt
 # 8192
-torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/8192_stf_0_align_0 --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_8192_8GPU.txt
+torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/8192 --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_8192_8GPU.txt
 # 3072000
-torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/3072000_stf_0_align_0 --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_3072000_8GPU.txt
+torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/3072000 --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_3072000_8GPU.txt
 # edm
 torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/edm_ncsnpp --ref=fid-refs/cifar10-32x32.npz --num 50000 --ckpt 100000 --gen_seed 1 > res_edm.txt
 
@@ -76,16 +53,16 @@ torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/edm_
 # ================ Train FFHQ ================
 # 128
 torchrun --standalone --nproc_per_node=8 train.py \
-  --outdir=training-runs --name ffhq_128_stf_0_align_0 \
+  --outdir=training-runs --name ffhq_128 \
   --data=datasets/ffhq-64x64.zip --cond=0 --arch=ddpmpp \
-  --pfgmv2=1 --batch 256 --align=0 --aug_dim 128 \
+  --pfgmpp=1 --batch 256 --aug_dim 128 \
   --cres=1,2,2,2 --lr=2e-4 --dropout=0.05 --augment=0.15
 # generate
 torchrun --standalone --nproc_per_node=8 generate.py \
-  --seeds=0-49999 --outdir=./training-runs/ffhq_128_stf_0_align_0 \
-  --ckpt 150000 --pfgmv2=1 --align=0 --aug_dim=128 --steps=40
+  --seeds=0-49999 --outdir=./training-runs/ffhq_128 \
+  --ckpt 150000 --pfgmpp=1 --aug_dim=128 --steps=40
 # fid evaluation
-torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/ffhq_128_stf_0_align_0 --ref=fid-refs/ffhq-64x64.npz --num 50000 --ckpt 150000 --gen_seed 1 > res_ffhq_ddpmpp_128.txt
+torchrun --standalone --nproc_per_node=8 fid.py calc --images=training-runs/ffhq_128 --ref=fid-refs/ffhq-64x64.npz --num 50000 --ckpt 150000 --gen_seed 1 > res_ffhq_ddpmpp_128.txt
 
 # train edm model in the edm_pfgm code base
 torchrun --standalone --nproc_per_node=8 train.py   --outdir=training-runs --name ffhq_edm_ddpmpp   --data=datasets/ffhq-64x64.zip --cond=0 --arch=ddpmpp   --batch 256   --cres=1,2,2,2 --lr=2e-4 --dropout=0.05 --augment=0.15
